cf18e0aed92cc25a7f7f5882658fc148f3ab68f6
Dokkae6949 <finnliry@gmail.com>
2025-08-10 19:28:30 +0200

    Revert "Update: Kanshi monitor layout"
    This reverts commit 2ef76eb2ba8251be469654ba862d453db64e0bf3.

--- hosts/evergarden/homes/kurisu/configuration/kanshi.nix
+++ hosts/evergarden/homes/kurisu/configuration/kanshi.nix
@@ -37,6 +37,46 @@
         };
       }
       { profile = {
+          name = "docked_single";
+          outputs = [
+            # Center monitor.
+            { criteria = "DP-2";
+              mode = "2560x1440@165.08Hz";
+              position = "0,0";
+              scale = 1.0;
+              adaptiveSync = true;
+            }
+          ];
+        };
+      }
+      { profile = {
+          name = "docked_multiple";
+          outputs = [
+            # Center monitor.
+            { criteria = "DP-2";
+              mode = "2560x1440@165.08Hz";
+              position = "1920,0";
+              scale = 1.0;
+              adaptiveSync = true;
+            }
+
+            # Left monitor.
+            { criteria = "DP-3";
+              mode = "1920x1080@60Hz";
+              position = "0,360";
+              scale = 1.0;
+            }
+
+            # Right monitor.
+            { criteria = "DP-4";
+              mode = "1920x1080@60Hz";
+              position = "4480,360";
+              scale = 1.0;
+            }
+          ];
+        };
+      }
+      { profile = {
           name = "laptop_docked_multiple";
           outputs = [
             # Laptop internal monitor.
