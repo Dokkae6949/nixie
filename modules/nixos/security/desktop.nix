977fd3fb135f3a2abc03f93022eb257ec9a8b828
Dokkae6949 <finnliry@gmail.com>
2025-09-22 13:42:34 +0200

    Fix(1) polkit and keyring

--- modules/nixos/security/desktop.nix
+++ modules/nixos/security/desktop.nix
@@ -14,37 +14,27 @@ in
 
   config = lib.mkIf cfg.enable {
     security = {
-      rtkit.enable = true;
       polkit.enable = true;
-      pam.services.swaylock = {};
+
+      pam.services.ly.enable = true;
+      pam.services.ly.enableGnomeKeyring = true;
     };
   
     services = {
-      passSecretService = {
-        enable = true;
-        package = pkgs.libsecret;
-      };
-
       gnome.gnome-keyring.enable = true;
     };
 
-    environment.systemPackages = with pkgs; [
-      polkit_gnome
-    ];
-
-    systemd = {
-      user.services.polkit-gnome-authentication-agent-1 = {
-        description = "polkit-gnome-authentication-agent-1";
-        wantedBy = [ "graphical-session.target" ];
-        wants = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-        serviceConfig = {
-            Type = "simple";
-            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-            Restart = "on-failure";
-            RestartSec = 1;
-            TimeoutStopSec = 10;
-          };
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
       };
     };
   };
