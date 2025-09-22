b349ee475e7987ed15b31dd3735c260ce9c2ffbf
Dokkae6949 <finnliry@gmail.com>
2025-09-22 14:29:48 +0200

    Fix(4) polkit and keyring

--- modules/nixos/security/desktop.nix
+++ modules/nixos/security/desktop.nix
@@ -31,7 +31,7 @@ in
       after = [ "graphical-session.target" ];
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon";
+        ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
         Restart = "on-failure";
         RestartSec = 1;
         TimeoutStopSec = 10;
